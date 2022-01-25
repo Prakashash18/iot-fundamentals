79637436f392e1c7acc22b5e6d1f5a2ebe35c190
prakashash18 <prakashash18@gmail.com>
2022-01-25 17:35:23 +0800

    Update job5.py

--- job5.py
+++ job5.py
@@ -7,13 +7,11 @@ dhtDevice = adafruit_dht.DHT11(board.D4)
  
 while True:
     try:
-        # Print the values to the serial port
         temperature_c = dhtDevice.temperature
         humidity = dhtDevice.humidity
-        print("Temp: {:.2f} C    Humidity: {}% ".format(temperature_c, humidity))
+        print("Temp: {:.1f} C    Humidity: {}% ".format(temperature_c, humidity))
  
     except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
         time.sleep(2.0)
         continue
