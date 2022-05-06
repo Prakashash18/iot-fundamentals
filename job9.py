1e47d3a60fa82e4ece3c08517528bd23be7ef9f5
prakashash18 <prakashash18@gmail.com>
2022-05-06 11:09:15 +0800

    Update job9.py

--- job9.py
+++ job9.py
@@ -6,18 +6,18 @@ import adafruit_dht
 
 dhtDevice = adafruit_dht.DHT11(board.D4)
 url = "https://api.thingspeak.com/update"
-
+api_key = "API_KEY_HERE"
 
 while True:
     try:
         # Print the values to the serial port
         temperature_c = dhtDevice.temperature
         humidity = dhtDevice.humidity
-        print("Temp: {:.2f} C    Humidity: {}% ".format(temperature_c, humidity$
-        payload = {'api_key' : 'J7ZLU278U2M2XJJU', 'field1' : temperature_c, 'f$
+        print("Temp: {:.2f} C    Humidity: {}% ".format(temperature_c, humidity)
+        payload = {'api_key' : api_key, 'field1' : temperature_c, 'field2': humidity}
         r = requests.get(url, params=payload)
     except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
+   
         print(error.args[0])
         time.sleep(2.0)
         continue
